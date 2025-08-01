37d32136df5162e0851d28961fcc5c6efaa3660b
nah9449 <nah9449@stern.nyu.edu>
2025-08-01 10:24:50 -0400

    Determine the winner

--- README.md
+++ README.md
@@ -2,19 +2,16 @@
 
 
 
-I added this sentence using the GitHub online interface to edit the file.
+## Usage 
+
+Play a game of rock paper scissors
+
+```sh
+# Only works if this files does not import from other local py files:
+python app/rps.py
 
 
-# My First Repo!
+# if this file imports from other local py files:
+python -m app.rps
+```
 
-This is the README.md file. It uses the markdown language.
-
-Here is a list:
-
-  + Item 1
-  + Item 2
-  + Item 3
-
-For more information about Markdown syntax, see the [Markdown Cheat Sheet](https://www.markdownguide.org/cheat-sheet/).
-
-Editing the read me 
--- app/rps.py
+++ app/rps.py
@@ -7,11 +7,26 @@ print("User Chose:",player_choice)
 
 import random
 
-VALID_OPTIONS= ["rock","paper,scissors"]
+VALID_OPTIONS= ["rock","paper","scissors"]
 
 computer_choice = random.choice(VALID_OPTIONS)
 print("COMPUTER CHOSE:", computer_choice)
 
-# todo: determine the winner
+# bottom of the app/rps.py file:
 
-print("WINNER: TODO")
+if player_choice == computer_choice:
+    result = "TIE GAME"
+elif player_choice == "rock" and computer_choice == "scissors":
+    result = "USER WINS"
+elif player_choice == "rock" and computer_choice == "paper":
+    result = "COMP WINS"
+elif player_choice == "scissors" and computer_choice == "rock":
+    result = "COMP WINS"
+elif player_choice == "scissors" and computer_choice == "paper":
+    result = "USER WINS"
+elif player_choice == "paper" and computer_choice == "rock":
+    result = "COMP WINS"
+elif player_choice == "paper" and computer_choice == "scissors":
+    result = "COMP WINS"
+
+print(result)
